30ca8fd87729e16d24a176591a82c12690b8efb0
youngpabi <pabiandrew77@gmail.com>
2023-06-06 13:53:23 +0000

    gun

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num0 in range(0, 10):
-    for digit2 in range(num0 + 1, 10):
+    for num1 in range(num0 + 1, 10):
         if num0 == 8 and num1 == 9:
             print("{}{}".format(num0, num1))
         else:
