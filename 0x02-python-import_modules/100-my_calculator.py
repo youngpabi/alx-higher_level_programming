532f9e75e3c6b56777e7b711d7bd0d2030243deb
youngpabi <pabiandrew77@gmail.com>
2023-06-08 13:33:12 +0000

    man

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    if len(argv) -2 != 2:
+    if len(argv) -1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         quit(1)
     a = int(argv[1])
