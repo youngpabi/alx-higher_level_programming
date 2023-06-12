1f8adf77c15a9db48bf25deea5ef43e8c66c58c7
youngpabi <pabiandrew77@gmail.com>
2023-06-12 13:23:36 +0000

    mazam

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    my_list = list(my_list)
-    if idx <= 0:
-        return None
-    elif idx > len(my_list):
-        return None
-    else:
-        print("Element from index {} is {}".format(idx, my_list[idx]))
+    return (my_list[idx] if 0 <= idx < len(my_list) else None)
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list = []):
     if my_list:
         my_list.reverse()
         for x in range(len(my_list)):
-            print("{:d}".format(my_list[index]))
+            print("{:d}".format(my_list[x]))
